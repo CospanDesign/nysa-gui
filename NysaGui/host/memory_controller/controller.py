9e36f61017b686a71a2eb0ae51ddce6ff29d023c
cospan <cospan@gmail.com>
2015-03-24 10:24:16 -0400

    Added a check to limit the size of a large memory read or write

--- NysaGui/host/memory_controller/controller.py
+++ NysaGui/host/memory_controller/controller.py
@@ -71,6 +71,9 @@ EPILOG = "\n" \
 "\t\t%s Something\n"\
 "\n" % n
 
+MAX_LONG_SIZE = 0x0800000
+#MAX_LONG_SIZE = 0x0200000
+
 class ReaderThread(QThread):
     def __init__(self, mutex, func):
         super(ReaderThread, self).__init__()
@@ -224,64 +227,108 @@ class Controller(NysaBaseController):
 
     def test_long_burst(self):
         status = "Passed"
-        size = self.n.get_device_size(self.urn)
-        if self.status.is_command_line():
-            self.status.Verbose( "Clearing Memory")
-            self.status.Verbose( "Memory Size: 0x%08X" % size)
-        data_out = Array('B')
-        for i in range(0, ((size / 4) - 1)):
-            num = 0x00
-            data_out.append(num)
-
-        self.n.write_memory(0, data_out)
-
-
-        if self.status.is_command_line():
-            self.status.Verbose( "long rw")
-        data_out = Array('B')
-        for i in range (0, size):
-            data_out.append((i % 255))
-
-        if self.status.is_command_line():
-            self.status.Verbose( "Writing 0x%08X bytes of data" % (len(data_out)))
-        start = time.time()
-        self.n.write_memory(0, data_out)
-        end = time.time()
-        if self.status.is_command_line(): 
-            self.status.Verbose( "Write Time : %f" % (end - start))
-            self.status.Verbose( "Reading 0x%08X bytes of data" % (len(data_out)))
-        start = time.time()
-        data_in = self.n.read_memory(0, len(data_out) / 4)
-        end = time.time()
-        if self.status.is_command_line():
-            self.status.Verbose( "Read Time: %f" % (end - start))
-            self.status.Verbose( "Comparing Values")
         fail = False
         fail_count = 0
-        if len(data_out) != len(data_in):
+        total_size = self.n.get_device_size(self.urn)
+        position = 0
+        if self.status.is_command_line():
+            self.status.Verbose( "Clearing Memory")
+            self.status.Verbose( "Memory Size: 0x%08X" % total_size)
+        size = 0
+        if total_size > MAX_LONG_SIZE:
+            self.status.Info("Memroy Size: 0x%08X is larger than read/write size" % total_size)
+            self.status.Info("\tBreaking transaction into 0x%08X chunks" % MAX_LONG_SIZE)
+            size = MAX_LONG_SIZE
+        else:
+            size = total_size
+
+        #Write Data Out
+        while position < total_size:
+            data_out = Array('B')
+            for i in range(0, ((size / 4) - 1)):
+                num = 0x00
+                data_out.append(num)
+
+            self.n.write_memory(0, data_out)
+
+
             if self.status.is_command_line():
-                self.status.Error( "Data in lenght not equal to data_out length")
-                self.status.Error( "\toutgoing: %d" % len(data_out))
-                self.status.Error( "\tincomming: %d" % len(data_in))
+                self.status.Verbose( "long rw")
+            data_out = Array('B')
+            for i in range (0, size):
+                data_out.append((i % 255))
 
-        
-        dout = data_out.tolist()
-        din = data_in.tolist()
+            if self.status.is_command_line():
+                self.status.Verbose( "Writing 0x%08X bytes of data" % (len(data_out)))
+            start = time.time()
+            self.n.write_memory(0, data_out)
+            end = time.time()
+            if self.status.is_command_line():
+                self.status.Verbose( "Write Time : %f" % (end - start))
+                self.status.Verbose( "Reading 0x%08X bytes of data" % (len(data_out)))
+
+            #Increment the position
+            prev_pos = position
+            position += size
+
+            if position + size > total_size:
+                size = total_size - position
+            else:
+                size += size
+
+            if self.status:
+                self.status.Info("Wrote: 0x%08X - 0x%08X" % (prev_pos, position))
 
 
-        for i in range(len(data_out)):
-            out_val = dout[i]
-            in_val = din[i]
-            if out_val != in_val:
-                print "%d and %d not equal" % (out_val, in_val)
-                fail = True
-                status = "Failed"
-                if self.status.is_command_line(): 
+        position = 0
+
+        start = time.time()
+        if total_size > MAX_LONG_SIZE:
+            self.status.Info("Memroy Size: 0x%08X is larger than read/write size" % total_size)
+            self.status.Info("\tBreaking transaction into 0x%08X chunks" % MAX_LONG_SIZE)
+
+            size = MAX_LONG_SIZE
+        else:
+            size = total_size
+
+ 
+        while position < total_size:
+
+            data_in = self.n.read_memory(0, len(data_out) / 4)
+            end = time.time()
+            if self.status.is_command_line():
+                self.status.Verbose( "Read Time: %f" % (end - start))
+                self.status.Verbose( "Comparing Values")
+            if len(data_out) != len(data_in):
+                if self.status.is_command_line():
+                    self.status.Error( "Data in lenght not equal to data_out length")
+                    self.status.Error( "\toutgoing: %d" % len(data_out))
+                    self.status.Error( "\tincomming: %d" % len(data_in))
+
+            dout = data_out.tolist()
+            din = data_in.tolist()
+
+            for i in range(len(data_out)):
+                out_val = dout[i]
+                in_val = din[i]
+                if out_val != in_val:
+                    fail = True
+                    status = "Failed"
+                    self.status.Error("%d and %d not equal" % (out_val, in_val))
                     self.status.Error("Mismatch @ 0x%08X: Write: (Hex): 0x%08X Read (Hex): 0x%08X" % (i, data_out[i], data_in[i]))
-                if fail_count >= 16:
-                    break
-                fail_count += 1
+                    if fail_count >= 16:
+                        break
+                    fail_count += 1
 
+            prev_pos = position
+            position += size
+            if position + size > total_size:
+                size = total_size - position
+            else:
+                size += position
+
+            if self.status:
+                self.status.Info("Read: 0x%08X - 0x%08X" % (prev_pos, position))
 
         return status
 
