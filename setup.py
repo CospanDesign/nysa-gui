5a1adc87fd65311b8d2b00a3cc0d5d84afd9c0d5
cospan <cospan@gmail.com>
2015-05-22 01:04:40 -0400

    Working on a nysa gui installer

---
+++ setup.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python
+
+import os
+import glob
+
+from setuptools import setup, find_packages
+from distutils.command.install import install as DistutilsInstall
+
+
+long_desc=open("README.md").read(),
+
+try:
+    #Try and convert the readme from markdown to pandoc
+    from pypandoc import convert
+    long_desc = convert("README.md", 'rst')
+except:
+    pass
+
+setup(
+    name='nysa-gui',
+    version='0.0.1',
+    description='Graphical FPGA Development and Interactin Environment',
+    author='Cospan Design',
+    author_email='dave.mccoy@cospandesign.com',
+    packages=find_packages('.'),
+    url="http://nysa.cospandesign.com",
+    package_data={'' : ["*.json", "*.txt, *.md"]},
+    data_files=[
+    ],
+    include_package_data = True,
+    long_description=long_desc,
+    entry_points = {
+        'console_scripts': ['nui=NysaGui.nysa_gui.nysa_gui:main'],
+    },
+    install_requires=[
+        'nysa >= 0.8.6',
+        'networkx >= 1.9.1',
+        'yaml >= 0.1.0',
+        'numpy >= 1.8.2',
+        'pyqtgraph >= 0.9.10'
+    ],
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "License :: OSI Approved :: MIT License",
+        "Environment :: X11 Applications :: Qt"
+        "Programming Language :: Python :: 2.7",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+    ],
+    keywords="FPGA",
+    license="GPL"
+)
